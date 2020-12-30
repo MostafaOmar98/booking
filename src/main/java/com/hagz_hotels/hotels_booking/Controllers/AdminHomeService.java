eeb74420ad4b67bf06f64b68e5c128dc41bcd2f6
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2020-12-30 09:06:31 +0200

    added template to gitignore

--- src/main/java/com/hagz_hotels/hotels_booking/Controllers/AdminHomeService.java
+++ src/main/java/com/hagz_hotels/hotels_booking/Controllers/AdminHomeService.java
@@ -1,4 +1,23 @@
 package com.hagz_hotels.hotels_booking.Controllers;
 
-public class AdminHomeService {
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/admin-home")
+public class AdminHomeService extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
