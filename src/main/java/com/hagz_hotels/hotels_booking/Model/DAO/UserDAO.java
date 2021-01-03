faab21f346a65cdbe2a7288764b0cc8f384c1710
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-03 17:51:58 +0200

    Refactor.

--- src/main/java/com/hagz_hotels/hotels_booking/Model/DAO/UserDAO.java
+++ src/main/java/com/hagz_hotels/hotels_booking/Model/DAO/UserDAO.java
@@ -6,34 +6,31 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.function.Function;
 
 public class UserDAO {
+
+    Function<ResultSet, User> mapper = new Function<ResultSet, User>() {
+        @Override
+        public User apply(ResultSet set) {
+
+            User user = new User();
+            try {
+                user.setUserId(set.getInt("UserId"));
+                user.setName(set.getString("Name"));
+                user.setEmail(set.getString("Email"));
+                user.setPhone(set.getString("Phone"));
+                user.setType(set.getString("Type"));
+                user.setPassword(set.getString("Password"));
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            return user;
+        }
+    };
+
     public User findByEmailAndPasswordAndType(String email, String password, String type) {
         String query = "SELECT * FROM User WHERE Email=? AND Password=? AND Type=?";
-        Connection con = DBUtil.getConnection();
-        User user = null;
-        try {
-            PreparedStatement stmt = con.prepareStatement(query);
-            stmt.setString(1, email);
-            stmt.setString(2, password);
-            stmt.setString(3, type);
-            ResultSet set = stmt.executeQuery();
-            if (set.next())
-                user = map(set);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return user;
-    }
-
-    private User map(ResultSet set) throws SQLException {
-        User user = new User();
-        user.setUserId(set.getInt("UserId"));
-        user.setName(set.getString("Name"));
-        user.setEmail(set.getString("Email"));
-        user.setPhone(set.getString("Phone"));
-        user.setType(set.getString("Type"));
-        user.setPassword(set.getString("Password"));
-        return user;
+        return DBUtil.selectOne(query, mapper, email, password, type);
     }
 }
