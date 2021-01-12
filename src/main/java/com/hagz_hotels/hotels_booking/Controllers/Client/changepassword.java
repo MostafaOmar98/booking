5a76a6f50b77dbcb99cd4adad843878b6276a0b2
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-01-12 21:10:29 +0200

    add changepassword servlet

---
+++ src/main/java/com/hagz_hotels/hotels_booking/Controllers/Client/changepassword.java
@@ -0,0 +1,27 @@
+package com.hagz_hotels.hotels_booking.Controllers.Client;
+
+import com.hagz_hotels.hotels_booking.Controllers.Auth;
+import com.hagz_hotels.hotels_booking.Model.DAO.UserDAO;
+import com.hagz_hotels.hotels_booking.Model.Entities.User;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "change-password", value = "/change-password")
+public class changepassword extends HttpServlet {
+//    User.Type[] authType = {User.Type.CLIENT};
+    User.Type authType = User.Type.CLIENT;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(!Auth.authenticate(request,response,authType)){
+            response.sendRedirect("index.jsp");
+            return ;
+        }
+        response.sendRedirect("change-password.jsp");
+
+    }
+
+}
