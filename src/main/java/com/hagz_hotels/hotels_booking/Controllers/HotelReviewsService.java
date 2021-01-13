3e9af5beef4c3b4658a3b2ffe7e1ec9f825fff9f
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-13 23:08:23 +0200

    rename of class.

--- src/main/java/com/hagz_hotels/hotels_booking/Controllers/HotelReviewsService.java
+++ src/main/java/com/hagz_hotels/hotels_booking/Controllers/HotelReviewsService.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/hotel-reviews")
-public class ViewReviewsService extends HttpServlet {
+public class HotelReviewsService extends HttpServlet {
 
     RoomDAO roomDAO;
     UserDAO userDAO;
