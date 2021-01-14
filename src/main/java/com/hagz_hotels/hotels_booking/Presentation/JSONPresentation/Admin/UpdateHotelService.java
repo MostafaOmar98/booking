2685bca63a6c2b3ac5f6bb0a26c3d161865a1a14
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-14 06:34:13 +0200

    Separation of business and presentation.

--- src/main/java/com/hagz_hotels/hotels_booking/Presentation/JSONPresentation/Admin/UpdateHotelService.java
+++ src/main/java/com/hagz_hotels/hotels_booking/Presentation/JSONPresentation/Admin/UpdateHotelService.java
@@ -1,9 +1,8 @@
 package com.hagz_hotels.hotels_booking.Presentation.JSONPresentation.Admin;
 
 
-import com.hagz_hotels.hotels_booking.Util.Auth;
-import com.hagz_hotels.hotels_booking.Model.DAO.HotelDAO;
-import com.hagz_hotels.hotels_booking.Model.Entities.User;
+import com.hagz_hotels.hotels_booking.Business.Admin.UpdateHotel;
+import com.hagz_hotels.hotels_booking.Presentation.JSONPresentation.Public.JSONAuth;
 import com.hagz_hotels.hotels_booking.Util.JsonResponse;
 
 import javax.servlet.ServletException;
@@ -12,23 +11,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/update-hotel")
 public class UpdateHotelService extends HttpServlet {
 
-    HotelDAO hotelDAO;
-    private final User.Type authType = User.Type.ADMIN;
-
-    @Override
-    public void init() throws ServletException {
-        hotelDAO = new HotelDAO();
-    }
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json");
-        if (!Auth.authenticateJson(request, response, authType))
-            return;
+        Integer hotelId = Integer.valueOf(request.getParameter("hotelId"));
+
+        try {
+            if (!JSONAuth.authorizeHotel(request, response, hotelId))
+                return;
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
 
         // Invalid input format will be validated by frontend. Bypass of frontend will result in status code 500 which is intended.
         String latitudeParam = request.getParameter("latitude"), longitudeParam = request.getParameter("longitude");
@@ -37,23 +35,16 @@ public class UpdateHotelService extends HttpServlet {
         Float longitude = longitudeParam == null ? null : Float.valueOf(longitudeParam);
         Integer adminId = adminIdParam == null ? null : Integer.valueOf(adminIdParam);
         String name = request.getParameter("name");
-        Integer hotelId = Integer.valueOf(request.getParameter("hotelId"));
-
-        if (!Auth.authorizeJsonHotel(request, response, hotelId))
-            return;
 
         JsonResponse jsonResponse = new JsonResponse();
-        if (name  != null && name.equals(""))
+        try {
+            UpdateHotel.execute(hotelId, name, latitude, longitude, request.getParameter("phone"), adminId);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
             jsonResponse.setAttr("name_error", "name_empty");
-        else {
-            hotelDAO.update(
-                    hotelId,
-                    request.getParameter("name"),
-                    latitude,
-                    longitude,
-                    request.getParameter("phone"),
-                    adminId
-            );
         }
 
         response.getWriter().println(jsonResponse);
