3657c6dd6736f3fcfef77ed4b29693dda54be619
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-01-14 18:44:32 +0200

    make validator abstract

---
+++ src/main/java/com/hagz_hotels/hotels_booking/Business/validators/MyValidator.java
@@ -0,0 +1,14 @@
+package com.hagz_hotels.hotels_booking.Business.validators;
+
+import javax.servlet.http.HttpServletRequest;
+
+public abstract class MyValidator {
+    String parameter;
+    abstract boolean validate(HttpServletRequest request) throws Exception;
+    boolean parameterIsEmpty(HttpServletRequest request){
+            return request.getAttribute(parameter) == null;
+    }
+
+
+
+}
