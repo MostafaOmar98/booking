bc96084c0ba6f8158b4708846c742b53ab1a6ddb
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-14 05:56:25 +0200

    Using new HTMLAuth.

--- src/main/java/com/hagz_hotels/hotels_booking/Presentation/HTMLPresentation/Client/ClientHomeService.java
+++ src/main/java/com/hagz_hotels/hotels_booking/Presentation/HTMLPresentation/Client/ClientHomeService.java
@@ -1,6 +1,6 @@
 package com.hagz_hotels.hotels_booking.Presentation.HTMLPresentation.Client;
 
-import com.hagz_hotels.hotels_booking.Util.Auth;
+import com.hagz_hotels.hotels_booking.Presentation.HTMLPresentation.Public.HTMLAuth;
 import com.hagz_hotels.hotels_booking.Model.Entities.User;
 
 import javax.servlet.ServletException;
@@ -13,14 +13,10 @@ import java.io.IOException;
 @WebServlet("/client-home")
 public class ClientHomeService extends HttpServlet {
     User.Type authType = User.Type.CLIENT;
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (!Auth.authenticate(request, response, authType))
+        if (!HTMLAuth.authorizeUserType(request, response, authType))
             return;
-
         request.getRequestDispatcher("/WEB-INF/client/client-home.jsp").forward(request, response);
     }
 }
