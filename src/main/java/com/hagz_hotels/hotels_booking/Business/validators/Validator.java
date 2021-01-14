12cb08a0c00d3e44e82c115da185b7e800e8dcd7
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-01-14 17:42:47 +0200

    throw exception in Validator

--- src/main/java/com/hagz_hotels/hotels_booking/Business/validators/Validator.java
+++ src/main/java/com/hagz_hotels/hotels_booking/Business/validators/Validator.java
@@ -3,7 +3,7 @@ package com.hagz_hotels.hotels_booking.Business.validators;
 import javax.servlet.http.HttpServletRequest;
 
 public interface Validator {
-    boolean validate(HttpServletRequest request);
+    boolean validate(HttpServletRequest request) throws Exception;
     boolean parameterIsEmpty(HttpServletRequest request);
 
 
