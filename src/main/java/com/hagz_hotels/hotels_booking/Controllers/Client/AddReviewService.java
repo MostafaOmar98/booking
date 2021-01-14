ed6d5db60a80a919cbf7b1955c20ea858eab63c0
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-14 02:10:12 +0200

    Add review backend.

---
+++ src/main/java/com/hagz_hotels/hotels_booking/Controllers/Client/AddReviewService.java
@@ -0,0 +1,41 @@
+package com.hagz_hotels.hotels_booking.Controllers.Client;
+
+
+import com.hagz_hotels.hotels_booking.Controllers.Auth;
+import com.hagz_hotels.hotels_booking.Model.DAO.ClientHotelReviewDAO;
+import com.hagz_hotels.hotels_booking.Model.Entities.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/add-review")
+public class AddReviewService extends HttpServlet {
+
+    ClientHotelReviewDAO clientHotelReviewDAO;
+    User.Type authType = User.Type.CLIENT;
+
+    @Override
+    public void init() throws ServletException {
+        clientHotelReviewDAO = new ClientHotelReviewDAO();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (!Auth.authenticate(request, response, authType))
+            return;
+        Integer reservationId = Integer.valueOf(request.getParameter("reservationId"));
+        if (!Auth.authorizeReservation(request, response, reservationId))
+            return;
+        Integer stars = Integer.valueOf(request.getParameter("stars"));
+        String comment = request.getParameter("comment");
+        Integer clientId = ((User)request.getSession().getAttribute("user")).getUserId();
+        if (!clientHotelReviewDAO.has(reservationId))
+            clientHotelReviewDAO.create(clientId, reservationId, stars, comment);
+        response.sendRedirect("client-reservations");
+    }
+}
