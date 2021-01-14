5623fbc7785d5b5bb29d7cd74c8b8565ccff4ada
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-14 07:14:02 +0200

    Separation of business and presentation.

--- src/main/java/com/hagz_hotels/hotels_booking/Presentation/JSONPresentation/Public/ValidateCredentialsService.java
+++ src/main/java/com/hagz_hotels/hotels_booking/Presentation/JSONPresentation/Public/ValidateCredentialsService.java
@@ -1,5 +1,6 @@
 package com.hagz_hotels.hotels_booking.Presentation.JSONPresentation.Public;
 
+import com.hagz_hotels.hotels_booking.Business.Public.SearchUser;
 import com.hagz_hotels.hotels_booking.Model.DAO.UserDAO;
 import com.hagz_hotels.hotels_booking.Model.Entities.User;
 import com.hagz_hotels.hotels_booking.Util.JsonResponse;
@@ -11,31 +12,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/validate-credentials")
 public class ValidateCredentialsService extends HttpServlet {
-
-    UserDAO userDAO;
-
-    @Override
-    public void init() throws ServletException {
-        userDAO = new UserDAO();
-    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json");
         String email = request.getParameter("email"), password = request.getParameter("password"), type = request.getParameter("type"); // TODO: username instead of email
-        User user = userDAO.findByEmailAndPasswordAndType(email, password, type);
+
         JsonResponse jsonResponse = new JsonResponse();
-        if (user != null)
-        {
-            HttpSession session = request.getSession();
-            session.setAttribute("user", user);
-            jsonResponse.setAttr("redirect", getServletContext().getContextPath() + "/index.jsp");
-        }
-        else
+        User user = null;
+        try {
+            user = SearchUser.execute(email, password, type);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
             jsonResponse.setAttr("error", "email_not_found");
+        }
+        HttpSession session = request.getSession();
+        session.setAttribute("user", user);
+        jsonResponse.setAttr("redirect", getServletContext().getContextPath() + "/index.jsp");
         response.getWriter().println(jsonResponse);
     }
 }
