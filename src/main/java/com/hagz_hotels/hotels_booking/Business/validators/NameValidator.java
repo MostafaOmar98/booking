eee6fcb0daeab1e959bbadb881c49e911fc72aa4
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-01-14 18:47:44 +0200

    add usernName Validator

---
+++ src/main/java/com/hagz_hotels/hotels_booking/Business/validators/NameValidator.java
@@ -0,0 +1,34 @@
+package com.hagz_hotels.hotels_booking.Business.validators;
+
+import com.hagz_hotels.hotels_booking.Model.DAO.UserDAO;
+import com.hagz_hotels.hotels_booking.Model.Entities.User;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class NameValidator extends MyValidator{
+    UserDAO userDAO = new UserDAO();
+    public NameValidator(){
+        parameter = "username";
+    }
+    @Override
+    public boolean validate(HttpServletRequest request) throws Exception {
+        if(parameterIsEmpty(request))
+            throw ValidationExceptionFactory.getUserNameException(Status.INVALID);
+        String userName = (String) request.getAttribute("username");
+        if(validUserName(userName)){
+            User user =userDAO.findByUserName(userName);
+            if(user == null){
+                return true;
+            }
+            throw ValidationExceptionFactory.getUserNameException(Status.USED);
+        }
+        throw ValidationExceptionFactory.getUserNameException(Status.INVALID);
+    }
+    enum Status{
+        USED,INVALID
+    }
+    Boolean validUserName(String username){
+        return true;
+    }
+
+}
