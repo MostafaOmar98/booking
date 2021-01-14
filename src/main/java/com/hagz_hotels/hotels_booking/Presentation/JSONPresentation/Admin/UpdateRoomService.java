4bbfc4177f252536770a9f0568a57288a8860e5d
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-14 07:00:21 +0200

    Separation of business and presentation.

--- src/main/java/com/hagz_hotels/hotels_booking/Presentation/JSONPresentation/Admin/UpdateRoomService.java
+++ src/main/java/com/hagz_hotels/hotels_booking/Presentation/JSONPresentation/Admin/UpdateRoomService.java
@@ -1,9 +1,8 @@
 package com.hagz_hotels.hotels_booking.Presentation.JSONPresentation.Admin;
 
 
-import com.hagz_hotels.hotels_booking.Util.Auth;
-import com.hagz_hotels.hotels_booking.Model.DAO.RoomDAO;
-import com.hagz_hotels.hotels_booking.Model.Entities.User;
+import com.hagz_hotels.hotels_booking.Business.Admin.UpdateRoom;
+import com.hagz_hotels.hotels_booking.Presentation.JSONPresentation.Public.JSONAuth;
 import com.hagz_hotels.hotels_booking.Util.JsonResponse;
 
 import javax.servlet.ServletException;
@@ -12,40 +11,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/update-room")
 public class UpdateRoomService extends HttpServlet {
 
-    RoomDAO roomDAO;
-    User.Type authType = User.Type.ADMIN;
-    @Override
-    public void init() throws ServletException {
-        roomDAO = new RoomDAO();
-    }
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json");
-        if (!Auth.authenticateJson(request, response, authType))
-            return;
-
+        Integer roomId = Integer.valueOf(request.getParameter("roomId"));
+        try {
+            if (!JSONAuth.authorizeRoom(request, response, roomId))
+                return;
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
 
         // Invalid input format will be handled by frontend. Frontend bypass will return 500 Internal Server Error which is intended.
-        Integer roomId = Integer.valueOf(request.getParameter("roomId")),
-                maxAdults = Integer.valueOf(request.getParameter("maxAdults")),
-                maxChildren = Integer.valueOf(request.getParameter("maxChildren"));
+        Integer maxAdults = Integer.valueOf(request.getParameter("maxAdults"));
+        Integer maxChildren = Integer.valueOf(request.getParameter("maxChildren"));
         Float pricePerNight = Float.valueOf(request.getParameter("pricePerNight"));
         String type = request.getParameter("type"),
                 facilities = request.getParameter("facilities");
 
-        if (!Auth.authorizeJsonRoom(request, response, roomId))
-            return;
 
         JsonResponse jsonResponse = new JsonResponse();
-        if (type.equals(""))
+        try {
+            UpdateRoom.execute(roomId, pricePerNight, type, maxAdults, maxChildren, facilities);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
             jsonResponse.setAttr("type_error", "type_empty");
-        else
-            roomDAO.update(roomId, pricePerNight, type, maxAdults, maxChildren, facilities);
+        }
         response.getWriter().println(jsonResponse);
     }
 }
