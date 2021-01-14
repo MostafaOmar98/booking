a5c5f6ba724318d26e1fd0de9ea58f37899237c6
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-14 07:03:37 +0200

    [Hanafy] Used new JSONAuth class, added Admin to authentication array, removed sendRedirect line, set content type to json.

--- src/main/java/com/hagz_hotels/hotels_booking/Presentation/JSONPresentation/Client/UpdatePassword.java
+++ src/main/java/com/hagz_hotels/hotels_booking/Presentation/JSONPresentation/Client/UpdatePassword.java
@@ -1,6 +1,6 @@
 package com.hagz_hotels.hotels_booking.Presentation.JSONPresentation.Client;
 
-import com.hagz_hotels.hotels_booking.Util.Auth;
+import com.hagz_hotels.hotels_booking.Presentation.JSONPresentation.Public.JSONAuth;
 import com.hagz_hotels.hotels_booking.Model.DAO.UserDAO;
 import com.hagz_hotels.hotels_booking.Model.Entities.User;
 import com.hagz_hotels.hotels_booking.Util.JsonResponse;
@@ -9,16 +9,18 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "update-password", value = "/update-password")
 public class UpdatePassword extends HttpServlet {
-    User.Type[] authType = {User.Type.CLIENT};
+    User.Type[] authType = {User.Type.CLIENT, User.Type.ADMIN}; // Added by bekh
     UserDAO userDAO = new UserDAO();
     /// todo extend authorization to give array of user types
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(!Auth.authenticate(request,response,authType)){
-            response.sendRedirect("index.jsp");
+        response.setContentType("application/json"); // added by bekh
+        if(!JSONAuth.authorizeUserType(request, response, authType[0], authType[1])){ // added by bekh
+//            response.sendRedirect("index.jsp"); COMMENTED BY BEKH
             return ;
         }
 
@@ -26,7 +28,13 @@ public class UpdatePassword extends HttpServlet {
         User user = (User)session.getAttribute("user");
         String newPassword = request.getParameter("newPassword");
 
-        userDAO.update(user.getName(), user.getEmail(),user.getPhone(),newPassword,user.getUserId());
+        try {
+            userDAO.update(user.getName(), user.getEmail(),user.getPhone(),newPassword,user.getUserId());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         JsonResponse jsonResponse = new JsonResponse();
         jsonResponse.setAttr("status", "password updated successfully");
         jsonResponse.setAttr("success", "true");
