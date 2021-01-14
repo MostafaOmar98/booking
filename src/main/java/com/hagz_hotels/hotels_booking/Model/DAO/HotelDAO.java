fac34c99ac2d432f4ec4a142fe8afbc84aa61fc6
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-14 04:20:06 +0200

    DB Classes now all throw exceptions.

--- src/main/java/com/hagz_hotels/hotels_booking/Model/DAO/HotelDAO.java
+++ src/main/java/com/hagz_hotels/hotels_booking/Model/DAO/HotelDAO.java
@@ -6,44 +6,38 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.function.Function;
 
 public class HotelDAO {
 
-    Function<ResultSet, Hotel> mapper = new Function<ResultSet, Hotel>() {
+    IMapper<ResultSet, Hotel> mapper = new IMapper<ResultSet, Hotel>() {
         @Override
-        public Hotel apply(ResultSet set) {
+        public Hotel apply(ResultSet set) throws SQLException {
             Hotel hotel = new Hotel();
-            try {
-                hotel.setHotelId(set.getInt("HotelId"));
-                hotel.setName(set.getString("Name"));
-                hotel.setLatitude(set.getFloat("Latitude"));
-                if (set.wasNull())
-                    hotel.setLatitude(null);
-                hotel.setLongitude(set.getFloat("Longitude"));
-                if (set.wasNull())
-                    hotel.setLongitude(null);
-                hotel.setPhone(set.getString("Phone"));
-                hotel.setAdminId(set.getInt("AdminId"));
-                return hotel;
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
+            hotel.setHotelId(set.getInt("HotelId"));
+            hotel.setName(set.getString("Name"));
+            hotel.setLatitude(set.getFloat("Latitude"));
+            if (set.wasNull())
+                hotel.setLatitude(null);
+            hotel.setLongitude(set.getFloat("Longitude"));
+            if (set.wasNull())
+                hotel.setLongitude(null);
+            hotel.setPhone(set.getString("Phone"));
+            hotel.setAdminId(set.getInt("AdminId"));
             return hotel;
         }
     };
 
-    public Hotel findByAdminId(Integer adminId) {
+    public Hotel findByAdminId(Integer adminId) throws SQLException, ClassNotFoundException {
         String query = "SELECT * FROM Hotel WHERE AdminId=?";
         return DBUtil.selectOne(query, mapper, adminId);
     }
 
-    public Integer create(String name, Integer adminId) {
+    public Integer create(String name, Integer adminId) throws SQLException, ClassNotFoundException {
         String query = "INSERT INTO Hotel (Name, AdminId) VALUES(?, ?)";
         return DBUtil.insert(query, name, adminId);
     }
 
-    public void update(Integer hotelId, String name, Float latitude, Float longitude, String phone, Integer adminId) {
+    public void update(Integer hotelId, String name, Float latitude, Float longitude, String phone, Integer adminId) throws SQLException, ClassNotFoundException {
         if (name != null)
             update(hotelId, "Name", name);
         if (latitude != null)
@@ -56,22 +50,22 @@ public class HotelDAO {
             update(hotelId, "AdminId", adminId);
     }
 
-    private void update(Integer hotelId, String fieldName, Object value) {
+    private void update(Integer hotelId, String fieldName, Object value) throws SQLException, ClassNotFoundException {
         String query = "UPDATE Hotel SET " + fieldName + "=? WHERE HotelId=?";
         DBUtil.executeUpdate(query, value, hotelId);
     }
 
-    public boolean has(Integer adminId, Integer hotelId) {
+    public boolean has(Integer adminId, Integer hotelId) throws SQLException, ClassNotFoundException {
         String query = "SELECT * FROM Hotel Where AdminId=? AND HotelId=?";
         return DBUtil.selectOne(query, mapper, adminId, hotelId) != null;
     }
 
-    public Hotel findById(Integer hotelId) {
+    public Hotel findById(Integer hotelId) throws SQLException, ClassNotFoundException {
         String query = "SELECT * FROM Hotel WHERE HotelId=?";
         return DBUtil.selectOne(query, mapper, hotelId);
     }
 
-    public List<Hotel> findByCriteria(Integer adults, Integer children, LocalDate checkIn, LocalDate checkOut) {
+    public List<Hotel> findByCriteria(Integer adults, Integer children, LocalDate checkIn, LocalDate checkOut) throws SQLException, ClassNotFoundException {
         String query = "SELECT * FROM Hotel WHERE EXISTS (" +
                 "SELECT * FROM Room WHERE " +
                 "Room.HotelId = Hotel.HotelId AND " +
