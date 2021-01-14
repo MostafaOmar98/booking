23055da8b8d1de31aee1708d051b7ffa98eba50d
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-14 04:15:01 +0200

    Use of IMapper instead of Function.

--- src/main/java/com/hagz_hotels/hotels_booking/Model/DAO/ClientHotelReviewDAO.java
+++ src/main/java/com/hagz_hotels/hotels_booking/Model/DAO/ClientHotelReviewDAO.java
@@ -5,41 +5,32 @@ import com.hagz_hotels.hotels_booking.Model.Entities.ClientHotelReview;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.function.Function;
 
 public class ClientHotelReviewDAO {
 
-    Function<ResultSet, ClientHotelReview> mapper = new Function<ResultSet, ClientHotelReview>() {
+    IMapper<ResultSet, ClientHotelReview> mapper = new IMapper<ResultSet, ClientHotelReview>() {
         @Override
-        public ClientHotelReview apply(ResultSet set) {
+        public ClientHotelReview apply(ResultSet set) throws SQLException {
             ClientHotelReview ret = new ClientHotelReview();
-            try {
-                ret.setClientId(set.getInt("ClientId"));
-                ret.setReservationId(set.getInt("ReservationId"));
-                ret.setCreatedAt(set.getTimestamp("CreatedAt").toLocalDateTime());
-                ret.setStars(set.getInt("Stars"));
-                ret.setComment(set.getString("Comment"));
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
+            ret.setClientId(set.getInt("ClientId"));
+            ret.setReservationId(set.getInt("ReservationId"));
+            ret.setCreatedAt(set.getTimestamp("CreatedAt").toLocalDateTime());
+            ret.setStars(set.getInt("Stars"));
+            ret.setComment(set.getString("Comment"));
             return ret;
         }
     };
 
-    Function<ResultSet, Float> avgRateMapper = new Function<ResultSet, Float>() {
+    IMapper<ResultSet, Float> avgRateMapper = new IMapper<ResultSet, Float>() {
         @Override
-        public Float apply(ResultSet set) {
+        public Float apply(ResultSet set) throws SQLException {
             Float ret = null;
-            try {
-                ret = set.getFloat("avg");
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
+            ret = set.getFloat("avg");
             return ret;
         }
     };
 
-    public List<ClientHotelReview> findByHotelId(Integer hotelId) {
+    public List<ClientHotelReview> findByHotelId(Integer hotelId) throws SQLException, ClassNotFoundException {
         String query = "SELECT * FROM ClientHotelReview, ClientRoomReservation, Room WHERE " +
                 "ClientHotelReview.ReservationId = ClientRoomReservation.ReservationId AND " +
                 "ClientRoomReservation.RoomId = Room.RoomId AND " +
@@ -47,7 +38,7 @@ public class ClientHotelReviewDAO {
         return DBUtil.selectAll(query, mapper, hotelId);
     }
 
-    public Float findAverageRatingByHotelId(Integer hotelId) {
+    public Float findAverageRatingByHotelId(Integer hotelId) throws SQLException, ClassNotFoundException {
         String query = "SELECT AVG(Stars) as avg FROM ClientHotelReview, ClientRoomReservation, Room WHERE " +
                 "ClientHotelReview.ReservationId = ClientRoomReservation.ReservationId AND " +
                 "ClientRoomReservation.RoomId = Room.RoomId AND " +
@@ -56,13 +47,13 @@ public class ClientHotelReviewDAO {
         return DBUtil.selectOne(query, avgRateMapper, hotelId);
     }
 
-    public void create(Integer clientId, Integer reservationId, Integer stars, String comment) {
+    public void create(Integer clientId, Integer reservationId, Integer stars, String comment) throws SQLException, ClassNotFoundException {
         String query = "INSERT INTO ClientHotelReview (ClientId, ReservationId, CreatedAt, Stars, Comment) VALUE " +
                 "(?, ?, NOW(), ?, ?)";
         DBUtil.executeUpdate(query, clientId, reservationId, stars, comment);
     }
 
-    public Boolean has(Integer reservationId) {
+    public Boolean has(Integer reservationId) throws SQLException, ClassNotFoundException {
         String query = "SELECT * FROM ClientHotelReview WHERE ReservationId=?";
         return DBUtil.selectOne(query, mapper, reservationId) != null;
     }
