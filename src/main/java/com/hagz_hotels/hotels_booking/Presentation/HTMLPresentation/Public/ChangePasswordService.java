fb51bed70e015e408844394fe8f5454827fd7b06
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-15 07:06:31 +0200

    ChangePassword now in public and allows for admin.

--- src/main/java/com/hagz_hotels/hotels_booking/Presentation/HTMLPresentation/Public/ChangePasswordService.java
+++ src/main/java/com/hagz_hotels/hotels_booking/Presentation/HTMLPresentation/Public/ChangePasswordService.java
@@ -1,6 +1,5 @@
-package com.hagz_hotels.hotels_booking.Presentation.HTMLPresentation.Client;
+package com.hagz_hotels.hotels_booking.Presentation.HTMLPresentation.Public;
 
-import com.hagz_hotels.hotels_booking.Presentation.HTMLPresentation.Public.HTMLAuth;
 import com.hagz_hotels.hotels_booking.Model.Entities.User;
 
 import javax.servlet.*;
@@ -10,12 +9,11 @@ import java.io.IOException;
 
 @WebServlet(name = "change-password", value = "/change-password")
 public class ChangePasswordService extends HttpServlet {
-//    User.Type[] authType = {User.Type.CLIENT};
-    User.Type authType = User.Type.CLIENT;
+    User.Type[] authType = {User.Type.CLIENT, User.Type.ADMIN};
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (!HTMLAuth.authorizeUserType(request, response, authType))
+        if (!HTMLAuth.authorizeUserType(request, response, authType[0], authType[1]))
             return;
         response.sendRedirect("change-password.jsp");
     }
