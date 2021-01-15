b6f684dbd4777c60fa2a35f7d2c2ce3b777a3a6d
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-15 21:06:42 +0200

    backend validation for rooms.

--- src/main/java/com/hagz_hotels/hotels_booking/Business/Admin/AddRoom.java
+++ src/main/java/com/hagz_hotels/hotels_booking/Business/Admin/AddRoom.java
@@ -11,6 +11,8 @@ public class AddRoom {
     public static void execute(Float pricePerNight, String type, Integer maxAdults, Integer maxChildren, Integer hotelId, String facilities) throws SQLException, ClassNotFoundException {
         if (type.isEmpty())
             throw new IllegalArgumentException("Type can't be empty");
+        if (pricePerNight < 0 || maxAdults < 0 || maxChildren < 0)
+            throw new IllegalArgumentException("Number can't be negative");
         roomDAO.create(
                 pricePerNight,
                 type,
--- src/main/java/com/hagz_hotels/hotels_booking/Business/Admin/UpdateRoom.java
+++ src/main/java/com/hagz_hotels/hotels_booking/Business/Admin/UpdateRoom.java
@@ -11,6 +11,8 @@ public class UpdateRoom {
     public static void execute(Integer roomId, Float pricePerNight, String type, Integer maxAdults, Integer maxChildren, String facilities) throws SQLException, ClassNotFoundException {
         if (type.isEmpty())
             throw new IllegalArgumentException("Type can't be empty");
+        if (pricePerNight < 0 || maxAdults < 0 || maxChildren < 0)
+            throw new IllegalArgumentException("Number can't be negative");
         roomDAO.update(roomId, pricePerNight, type, maxAdults, maxChildren, facilities);
     }
 }
